dfbf847d984b4aeb22265c153cbc5fd87e153e43
vDome <d.vaiano1@studenti.unisa.it>
2024-11-28 15:04:57 +0100

    Initial commit with project files

---
+++ src/main/java/com/example/helpmeup/HelpMeUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.helpmeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpMeUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpMeUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helpmeup/HelpMeUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helpmeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpMeUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
